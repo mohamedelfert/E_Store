d39c3b114a1f3509cf88a1faa0432c5bccd02cd7
mohamedelfert <me_me4152@yahoo.com>
2019-12-23 00:53:34 +0200

    create two files in users language errors and labels

---
+++ app/languages/ar/users/labels.lang.php
@@ -0,0 +1,8 @@
+<?php
+$_['text_label_Username']                  = 'إسم المستخدم';
+$_['text_label_Email']                = 'البريد الإلكتروني';
+$_['text_label_CEmail']        = 'تأكيد البريد الإلكتروني';
+$_['text_label_PhoneNumber']         = 'رقم الهاتف';
+$_['text_label_Password']             = 'كلمه المرور';
+$_['text_label_CPassword']     = 'تأكيد كلمه المرور';
+$_['text_label_GroupId']              = 'إختر المجموعه ؟';
