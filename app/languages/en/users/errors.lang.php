003e37507b0e7a741ad20c12b8a6a9500bbd4f45
mohamedelfert <me_me4152@yahoo.com>
2019-12-23 20:51:05 +0200

    add two files in users language errors and labels in en language

---
+++ app/languages/en/users/errors.lang.php
@@ -0,0 +1,3 @@
+<?php
+$_['text_error_required']           = 'Should Contain Data';
+$_['text_error_min']                = 'Number Of Characters Less Than Required';
---
+++ app/languages/en/users/labels.lang.php
@@ -0,0 +1,8 @@
+<?php
+$_['text_label_Username']           = 'UserName';
+$_['text_label_Email']              = 'Email';
+$_['text_label_CEmail']             = 'Confirm Email';
+$_['text_label_PhoneNumber']        = 'Phone Number';
+$_['text_label_Password']           = 'Password';
+$_['text_label_CPassword']          = 'Confirm Password';
+$_['text_label_GroupId']            = 'Select Group ?';
