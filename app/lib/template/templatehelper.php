888e58901ac91457602ba282fbf505947fa64d3e
mohamedelfert <me_me4152@yahoo.com>
2019-12-29 00:27:56 +0200

    ى

--- app/lib/template/templatehelper.php
+++ app/lib/template/templatehelper.php
@@ -9,11 +9,13 @@ trait TemplateHelper
        return parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH) === $url;
     }
 
-    public function showValue($fieldName, $object = null){
+    public function showValue($fieldName, $object = null)
+    {
         return isset($_POST[$fieldName]) ? $_POST[$fieldName] : (is_null($object) ? '' : $object->$fieldName);
     }
 
-    public function selectedGroup($fieldName,$value, $object = null){
+    public function selectedGroup($fieldName,$value, $object = null)
+    {
         return (isset($_POST[$fieldName]) && $_POST[$fieldName] == $value) || ((!is_null($object) && $object->$fieldName == $value)) ? 'selected="selected"' : '';
     }
 }
