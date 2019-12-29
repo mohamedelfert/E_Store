042707f5aa35573ea215f2bf4217f405855dbcf7
mohamedelfert <me_me4152@yahoo.com>
2019-12-29 22:01:38 +0200

    add new text messages

--- app/languages/ar/users/messages.lang.php
+++ app/languages/ar/users/messages.lang.php
@@ -1,3 +1,9 @@
 <?php
-$_['text_message_success']     = 'تم حفظ المستخدم بنجاح';
-$_['text_message_failed']      = 'خطأ أثناء حفظ المستخدم';
+$_['text_message_success']          = 'تم حفظ المستخدم بنجاح';
+$_['text_message_update']           = 'تم تحديث البيانات بنجاح';
+$_['text_message_delete_success']   = 'تم حذف المستخدم بنجاح';
+$_['text_message_delete_failed']    = 'خطأ اثناء حذف المستخدم';
+$_['text_message_exist_username']   = 'اسم المستخدم مسجل مسبقا';
+$_['text_message_exist_email']      = 'البريد الالكتروني مسجل مسبقا';
+$_['text_message_exist_phone']      = 'الهاتف مسجل مسبقا';
+$_['text_message_failed']           = 'خطأ أثناء حفظ المستخدم';
--- app/languages/en/users/messages.lang.php
+++ app/languages/en/users/messages.lang.php
@@ -1,3 +1,9 @@
 <?php
-$_['text_message_success']     = 'User Saved Successfully';
-$_['text_message_failed']      = 'Error Saving User';
+$_['text_message_success']          = 'User Saved Successfully';
+$_['text_message_update']           = 'Data Updated Successfully';
+$_['text_message_delete_success']   = 'User Deleted Successfully';
+$_['text_message_delete_failed']    = 'Error Deleting User';
+$_['text_message_exist_username']   = 'Username Already Exist';
+$_['text_message_exist_email']      = 'Email Already Exist';
+$_['text_message_exist_phone']      = 'Phone Number Already Exist';
+$_['text_message_failed']           = 'Error Saving User';
