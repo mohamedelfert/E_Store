54cd19fabb1f05091c35b662d4840792d684b31c
mohamedelfert <me_me4152@yahoo.com>
2019-12-30 12:57:01 +0200

    edit

--- app/languages/ar/auth/login.lang.php
+++ app/languages/ar/auth/login.lang.php
@@ -2,5 +2,5 @@
 $_['text_Username']       = 'اسم المستخدم';
 $_['text_Password']       = 'كلمة المرور';
 $_['text_button']         = 'دخول';
-$_['text_forgotpassword'] = 'هل نسيت كلمة السر ؟';
+$_['text_forgot_password'] = 'هل نسيت كلمة السر ؟';
 $_['text_login_title']    = 'تسجيل الدخول';
