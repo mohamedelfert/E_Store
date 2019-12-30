4e1032d44a97cdb87f0d1b468af557d8fc934beb
mohamedelfert <me_me4152@yahoo.com>
2019-12-30 12:56:44 +0200

    edit login form

--- app/views/auth/login.view.php
+++ app/views/auth/login.view.php
@@ -1,22 +1,15 @@
-<div id="container">
-    <div class="wrapper fadeInDown">
-        <div id="formContent">
-            <!-- Tabs Titles -->
-            <h1><?= $text_login_title ?></h1>
-            <!-- Icon -->
-            <div class="fadeIn first">
-                <img src="/images/non-avatar.png" width="20" height="20" id="icon" alt="User Icon" />
-            </div>
-            <!-- Login Form -->
-            <form class="app_form" method="post" enctype="application/x-www-form-urlencoded">
-                <input type="text" id="Username" class="fadeIn second" name="Username" placeholder="<?= $text_Username ?>">
-                <input type="password" id="Password" class="fadeIn third" name="Password" placeholder="<?= $text_Password ?>">
-                <input type="submit" name="submit" class="fadeIn fourth" value="<?= $text_button ?>">
-            </form>
-            <!-- Remind Password -->
-            <div id="formFooter">
-                <a class="underlineHover" href="#"><?= $text_forgotpassword ?></a>
-            </div>
-        </div>
+<div id="container" class="login">
+    <div class="card card-container">
+        <h1><?= $text_login_title ?></h1>
+        <img id="profile-img" class="profile-img-card" src="/images/non-avatar.png" />
+        <p id="profile-name" class="profile-name-card"></p>
+        <form class="form-signin" method="post" enctype="application/x-www-form-urlencoded">
+            <input type="text" id="Username" name="Username" class="form-control" placeholder="<?= $text_Username ?>" required>
+            <input type="password" id="Password" name="Password" class="form-control" placeholder="<?= $text_Password ?>" required>
+            <input class="btn btn-lg btn-primary btn-block btn-signin" type="submit" name="submit" value="<?= $text_button ?>">
+        </form>
+        <a href="#" class="forgot-password">
+            <?= $text_forgot_password ?>
+        </a>
     </div>
 </div>
