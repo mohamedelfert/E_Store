abc33454a20f3e6cfcba44686c1b9d94ee14af0e
mohamedelfert <me_me4152@yahoo.com>
2019-12-30 14:24:43 +0200

    edit loginAction and create logoutAction

--- app/controllers/authcontroller.php
+++ app/controllers/authcontroller.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace PHPMVC\Controllers;
+use PHPMVC\LIB\Helper;
+use PHPMVC\LIB\Messenger;
 use PHPMVC\Models\UsersModel;
 
 class AuthController extends AbstractController
 {
+    use Helper;
     public function loginAction()
     {
         $this->language->load('auth.login');
@@ -12,9 +15,22 @@ class AuthController extends AbstractController
             ':view'        => ':action_view',
         ]);
         if (isset($_POST['submit'])){
-            UsersModel::authenticate($_POST['Username'] , $_POST['Password'] , $this->session);
+            $isAuthorized = UsersModel::authenticate($_POST['Username'] , $_POST['Password'] , $this->session);
+            if ($isAuthorized == 2){
+                $this->messenger->add($this->language->get('text_user_disabled') , Messenger::APP_MESSAGE_INFO);
+            }elseif ($isAuthorized == 1){
+                $this->redirect('/');
+            }elseif ($isAuthorized == false){
+                $this->messenger->add($this->language->get('text_user_not_found') , Messenger::APP_MESSAGE_ERROR);
+            }
         }
         $this->_view();
+    }
+
+    public function logoutAction()
+    {
+        $this->session->kill();
+        $this->redirect('/auth/login');
     }
 
 }
