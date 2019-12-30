fa311a2e09c1e4f35a87a06235b650be33a4309a
mohamedelfert <me_me4152@yahoo.com>
2019-12-30 14:23:33 +0200

    add more text

--- app/languages/ar/auth/login.lang.php
+++ app/languages/ar/auth/login.lang.php
@@ -1,6 +1,8 @@
 <?php
-$_['text_Username']       = 'اسم المستخدم';
-$_['text_Password']       = 'كلمة المرور';
-$_['text_button']         = 'دخول';
-$_['text_forgot_password'] = 'هل نسيت كلمة السر ؟';
-$_['text_login_title']    = 'تسجيل الدخول';
+$_['text_Username']             = 'اسم المستخدم';
+$_['text_Password']             = 'كلمة المرور';
+$_['text_button']               = 'دخول';
+$_['text_forgot_password']      = 'هل نسيت كلمة السر ؟';
+$_['text_login_title']          = 'تسجيل الدخول';
+$_['text_user_disabled']        = 'عفوا هذا المستخدم تم تعطيله من فضلك راجع الاداره';
+$_['text_user_not_found']       = 'خطأ في اسم المستخدم او كلمة المرور';
--- app/languages/en/auth/login.lang.php
+++ app/languages/en/auth/login.lang.php
@@ -1,6 +1,8 @@
 <?php
-$_['text_Username']       = 'Username';
-$_['text_Password']       = 'Password';
-$_['text_button']         = 'Login';
-$_['text_forgotpassword'] = 'Forgot Password ?';
-$_['text_login_title']    = 'LogIn';
+$_['text_Username']             = 'Username';
+$_['text_Password']             = 'Password';
+$_['text_button']               = 'Login';
+$_['text_forgot_password']      = 'Forgot Password ?';
+$_['text_login_title']          = 'LogIn';
+$_['text_user_disabled']        = 'Sorry This User Has Been Disabled, Please See Administration';
+$_['text_user_not_found']       = 'An Error In Username Or Password';
