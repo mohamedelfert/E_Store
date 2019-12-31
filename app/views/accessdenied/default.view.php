8a5e1e4d3860cbf0648f56bf7e32344eec3906f1
mohamedelfert <me_me4152@yahoo.com>
2019-12-31 18:28:13 +0200

    create AccessDeniedController  and create the view for it

---
+++ app/views/accessdenied/default.view.php
@@ -0,0 +1,7 @@
+<div id="container">
+    <div class="container-fluid">
+        <div class="text-center">
+            <p class="lead text-gray-800 mb-5"><?= $text_access_denied ?></p>
+        </div>
+    </div>
+</div>
