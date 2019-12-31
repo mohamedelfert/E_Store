1d7ddd22b38138d1004ab54b7213798211e6f6e4
mohamedelfert <me_me4152@yahoo.com>
2019-12-31 18:25:50 +0200

    add variable $_excludedRoutes as array include some urls and create function hasAccess to check the url in this array $_excludedRoutes or in $this->_session->user->privileges then return true

--- app/lib/authentication.php
+++ app/lib/authentication.php
@@ -6,6 +6,16 @@ class Authentication
 {
     private static $_instance;
     private $_session;
+    private $_excludedRoutes = [
+        '/index/default',
+        '/auth/logout',
+        '/users/profile',
+        '/users/changepassword',
+        '/language/default',
+        '/users/settings',
+        '/accessdenied/default',
+        '/test/default',
+    ];
 
     private function __construct($session)
     {
@@ -24,5 +34,12 @@ class Authentication
 
     public function isAuthorized(){
         return isset($this->_session->user);
+    }
+
+    public function hasAccess($controller , $action){
+        $url = strtolower('/' . $controller . '/' . $action);
+        if (in_array($url , $this->_excludedRoutes) || in_array($url , $this->_session->user->privileges)){
+            return true;
+        }
     }
 }
